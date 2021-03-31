7fbfbea9aeedca6b574f6ebbcded52efbc14c43b
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2021-03-31 21:52:31 +0100

    Use HTML audio tag for music player

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { PlayerStateService } from '../player-state.service';
-import { SongObj, SongsService } from '../songs.service';
+import { SongInterface, SongObj, SongsService } from '../songs.service';
 
 @Component({
   selector: 'app-player',
@@ -9,13 +8,42 @@ import { SongObj, SongsService } from '../songs.service';
 })
 export class PlayerComponent implements OnInit {
 
-  constructor(private player: PlayerStateService) { }
+  songObjs: SongObj[] = [];
+
+  // Default load Welcome Horizons
+  currentSong: SongObj = {
+    id: 95,
+    name: 'Welcome Horizons',
+    music_uri: 'https://acnhapi.com/v1/music/95',
+    image_uri: 'https://acnhapi.com/v1/images/songs/95'
+  };
+
+  constructor(private songService: SongsService) { }
 
   ngOnInit(): void {
-    this.player.init();
+    this.getAllSongs();
   }
 
-  songs: SongObj[] = this.player.songs;
+  private getAllSongs = () => this.songService.getAllSongs().subscribe({
+    error: (_) => console.log('Error fetching all song information', _),
+    next: (data: Object) => this.extractSongs(data)
+  });
+  
+  private extractSongs = (songs: Object): void => {
+    this.songObjs = Object.entries(songs).map(song => {
+      const [_, songMetaData] = song;
 
-  currentSong: SongObj = this.player.currentSong;
+      // SongInterface lets us select keys on the object
+      // Return type of SongObj instead of SongInterface means
+      // we only need to save the information we specify, and not the whole object
+      const songI: SongInterface = songMetaData as SongInterface;
+
+      return {
+        id: songI.id,
+        name: songI.name['name-EUen'],
+        music_uri: songI.music_uri,
+        image_uri: songI.image_uri
+      }
+    })    
+  }
 }
--- src/app/songs.service.ts
+++ src/app/songs.service.ts
@@ -6,7 +6,7 @@ interface NamesInterface {
   'name-EUen': string
 }
 
-interface SongInterface {
+export interface SongInterface {
   id: number,
   name: NamesInterface
   music_uri: string
@@ -30,29 +30,7 @@ export class SongsService {
 
   constructor(private http: HttpClient) { }
 
-  songObjs: SongObj[] = [];
-
-  getAllSongs = (): Subscription => {
-    return this.http.get(this.baseUrl + '/songs').subscribe({
-      error: (_) => console.log('Error fetching all song information', _),
-      next: (data: Object) => this.extractSongs(data)
-    });
-  }
-
-  private extractSongs = (songs: Object): void => {
-    this.songObjs = Object.entries(songs).map(song => {
-      const [_, songMetaData] = song;
-
-      // SongInterface lets us select keys on the object
-      // Return type of SongObj instead of SongInterface means
-      // we only need to save the information we specify, and not the whole object
-      const songI: SongInterface = songMetaData as SongInterface;
-      return {
-        id: songI.id,
-        name: songI.name['name-EUen'],
-        music_uri: songI.music_uri,
-        image_uri: songI.image_uri
-      }
-    })    
+  getAllSongs = () => {
+    return this.http.get(this.baseUrl + '/songs')
   }
 }
